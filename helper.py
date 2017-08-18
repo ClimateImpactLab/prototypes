42368761f995382b51e6937b8c312fadeef8908c
jgerardsimcock <justin_simcock@yahoo.com>
2017-08-18 10:07:42 -0700

    update for brc config

--- helper.py
+++ helper.py
@@ -23,17 +23,20 @@ __version__ = '0.1.0'
 
 # growth_path = '/global/scratch/jsimcock/social/baselines/gdppc-growth.csv'
 
-# write_path_brc = '/global/scratch/jsimcock/data_files/covars/ssp_gdppc/{ssp}/{model}/{year}/{version}.nc'
+write_path_brc = '/global/scratch/jsimcock/data_files/covars/ssp_kernel_' + kernel +
+                '_gdppc/{ssp}/{model}/{year}/{version}.nc'
 
-# covar_path_brc = '/global/scratch/jsimcock/data_files/covars/ssp_gdppc/{ssp}/{model}/{year}/{version}.nc'
+covar_path_brc = '/global/scratch/jsimcock/data_files/covars/ssp_gdppc/{ssp}/{model}/{year}/{version}.nc'
 
-write_path = '/Users/justinsimcock/data/gdps/ssp_kernel_gddpc/{ssp}/{model}/{year}/{version}.nc'
+# write_path = '/Users/justinsimcock/data/gdps/ssp_kernel_gddpc/{ssp}/{model}/{year}/{version}.nc'
 
-covar_path = '/Users/justinsimcock/data/gdps/ssp_gddpc_{ssp}_{model}_{year}_1.0.nc'
+# covar_path = '/Users/justinsimcock/data/gdps/ssp_gddpc_{ssp}_{model}_{year}_1.0.nc'
 
 ssps = ['SSP1', 'SSP2', 'SSP3', 'SSP4', 'SSP5']
 
 models = ['low', 'high']
+
+kernel = 13
 
 
 mdata = dict(
@@ -52,11 +55,11 @@ if __name__ == '__main__':
     mdata['model'] = model
 
     for y in range(2010, 2100):
-      window = range(y-12, y+1)
+      window = range(y-(kernel-1), y+1)
       #print(window)
-      paths = [covar_path.format(ssp=ssp, model=model,year=yr) for yr in window]
+      paths = [covar_path_brc.format(ssp=ssp, model=model,year=yr) for yr in window]
       #print(paths)
       mdata['dependencies'] = str(paths)
       mdata['year'] = y 
 
-      gen_kernel_covars(paths, kernel = 13, metadata=mdata, write_path=write_path)
+      gen_kernel_covars(paths, kernel = kernel, metadata=mdata, write_path=write_path_brc)
