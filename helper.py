5618c54e2b0b6a2d83af5a313b8c20296b1d4ff8
jgerardsimcock <justin_simcock@yahoo.com>
2017-08-21 21:08:06 -0700

    update helper

--- helper.py
+++ helper.py
@@ -143,7 +143,7 @@ def gen_covars(
         logger.debug('attempting to compute kernel climate covariate for year {}'.format(y))
 
         ds = gen_smoothed_covars(datasets, dim='year', kernel=kernel)
-
+        print(ds)
 
         # ds.attrs.update({k: str(v) for k, v in metadata.items()})
 
