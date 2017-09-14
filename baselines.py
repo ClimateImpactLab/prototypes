1e2d5d6185b9d19f90a93414aacf566628e0e5de
jgerardsimcock <justin_simcock@yahoo.com>
2017-09-14 13:54:49 -0700

    ready to test subclass

--- baselines.py
+++ baselines.py
@@ -202,66 +202,66 @@ def _findpolymin(coeffs, min_max):
 
     return possibles[index]
 
-def compute_m_star(betas, min_function=_findpolymins, min_max=[10,25], write_path=None):
-    '''
-    Computes m_star, the value of an impact function for a given set of betas given t_star. 
-    t_star, the value t at which an impact is minimized for a given hierid is precomputed 
-    and used to compute m_star.
+# def compute_m_star(betas, min_function=_findpolymins, min_max=[10,25], write_path=None):
+#     '''
+#     Computes m_star, the value of an impact function for a given set of betas given t_star. 
+#     t_star, the value t at which an impact is minimized for a given hierid is precomputed 
+#     and used to compute m_star.
 
-    Parameters
-    ----------
-    betas: :py:class `~xarray.Dataset` 
-        coefficients by hierid Dataset 
+#     Parameters
+#     ----------
+#     betas: :py:class `~xarray.Dataset` 
+#         coefficients by hierid Dataset 
 
-    min_max: np.array
-        values to evaluate min at 
+#     min_max: np.array
+#         values to evaluate min at 
 
-    min_function: minimizing function to compute tstar
+#     min_function: minimizing function to compute tstar
 
-    write_path: str
+#     write_path: str
 
-    Returns
-    -------
+#     Returns
+#     -------
 
-    m_star Dataset
-        :py:class`~xarray.Dataset` of impacts evaluated at tstar. 
+#     m_star Dataset
+#         :py:class`~xarray.Dataset` of impacts evaluated at tstar. 
 
 
-    .. note:: writes to disk and subsequent calls will read from disk. 
-    '''
-    #if file does not exist create it
-    if not os.path.isfile(write_path):
+#     .. note:: writes to disk and subsequent calls will read from disk. 
+#     '''
+#     #if file does not exist create it
+#     if not os.path.isfile(write_path):
 
-        #Compute t_star according to min function
-        t_star = np.apply_along_axis(min_function, 1, betas, min_max)
+#         #Compute t_star according to min function
+#         t_star = np.apply_along_axis(min_function, 1, betas, min_max)
 
-        #Compute the weather dataset with t_star as base weather var
-        t_star_poly = xr.Dataset()
-        for i, pred in enumerate(betas.prednames.values):
-            t_star_poly['{}'.format(pred)] = xr.DataArray(t_star**(i+1), 
-                                            coords={'hierid': betas['hierid'], 'outcome': betas['outcome']}, 
-                                            dims=['outcome', 'hierid']
-                                            )
+#         #Compute the weather dataset with t_star as base weather var
+#         t_star_poly = xr.Dataset()
+#         for i, pred in enumerate(betas.prednames.values):
+#             t_star_poly['{}'.format(pred)] = xr.DataArray(t_star**(i+1), 
+#                                             coords={'hierid': betas['hierid'], 'outcome': betas['outcome']}, 
+#                                             dims=['outcome', 'hierid']
+#                                             )
 
-        #write to disk
-        if not os.path.isdir(os.path.dirname(write_path)):
-                os.path.makedir(os.path.dirname(write_path))
+#         #write to disk
+#         if not os.path.isdir(os.path.dirname(write_path)):
+#                 os.path.makedir(os.path.dirname(write_path))
 
-        t_star_poly.to_netcdf(write_path)
+#         t_star_poly.to_netcdf(write_path)
 
-    #Read from disk
-    t_star_poly = _get_t_star(write_path)
+#     #Read from disk
+#     t_star_poly = _get_t_star(write_path)
 
     
-    return sum((t_star_poly*betas).data_vars.values()).sum(dim='prednames')
+#     return sum((t_star_poly*betas).data_vars.values()).sum(dim='prednames')
 
-@memoize
-def _get_t_star(path):
-    '''
-    Returns cached verison of t_star
+# @memoize
+# def _get_t_star(path):
+#     '''
+#     Returns cached verison of t_star
 
-    '''
-    with xr.open_dataset(path) as ds:
-        ds.load()
-    return ds
+#     '''
+#     with xr.open_dataset(path) as ds:
+#         ds.load()
+#     return ds
 
