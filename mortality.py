f313894f8050b34997551e2f35d440026d377ed8
jgerardsimcock <justin_simcock@yahoo.com>
2017-09-14 18:12:37 -0700

    debug m_star

--- mortality.py
+++ mortality.py
@@ -10,7 +10,9 @@ class Mortality_Polynomial(Impact):
 	'''
 	Mortality specific 
 	'''
-	self.min_function = minimize_polynomial
+	def __init__(self):
+		self.min_function = minimize_polynomial
+
 
 	def impact_function(self, betas, weather):
 		'''
