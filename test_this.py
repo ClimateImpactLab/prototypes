3891cf4dd4922b2d9ec34223ca9bf0b69f928df0
jgerardsimcock <justin_simcock@yahoo.com>
2017-09-15 13:44:34 -0700

    add class attr

--- test_this.py
+++ test_this.py
@@ -2,11 +2,13 @@ import xarray as xr, pandas as pd, numpy as np
 from mortality import Mortality_Polynomial
 from csvv import Gammas
 from impact import Impact
+import time
 
 
 
 def test_this():
 
+	t1  = time.time()
 	g = Gammas('/global/scratch/jsimcock/data_files/covars/global_interaction_Tmean-POLY-4-AgeSpec.csvv')
 	gdp = xr.open_dataset('/global/scratch/jsimcock/data_files/covars/ssp_kernel_13_gdppc/SSP1/high/2015/0.1.0.nc')
 	climtas = xr.open_dataset('/global/scratch/jsimcock/data_files/covars/climate/hierid/popwt/tas_kernel_30/rcp45/ACCESS1-0/2015/0.1.1.nc4')
@@ -21,10 +23,14 @@ def test_this():
 
 	m = Mortality_Polynomial(path, gammas.prednames.values)
 
-	impact, m_star = m.compute(gammas, gdp, climtas, min_max_boundary=[10,25], t_star_write_path=t_star_path) 
+	impact = m.compute(gammas, gdp, climtas, min_max_boundary=[10,25], t_star_write_path=t_star_path) 
 
-	
-	return impact, m_star       
+
+	t2 = time.time()
+
+	print(t2-t1)
+
+	return impact       
 
 
 
