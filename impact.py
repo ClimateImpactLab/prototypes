f76e7caf8411a60566d1359524ec7657b4f53e51
jgerardsimcock <justin_simcock@yahoo.com>
2017-09-14 17:42:38 -0700

    edit comput_m_star

--- impact.py
+++ impact.py
@@ -116,7 +116,7 @@ class Impact(object):
     impact= self.impact_function(betas, self.weather)
 
     #Compute the min for flat curve adaptation
-    m_star = self._compute_m_star(betas, min_function, min_max_boundary, t_star_write_path)
+    m_star = self._compute_m_star(betas, self.min_function, min_max_boundary, t_star_write_path)
       #Compare values and evaluate a max
     #impact = np.minimum(impact, m_star)
 
@@ -168,7 +168,7 @@ class Impact(object):
       if not os.path.isfile(t_star_write_path):
 
           #Compute t_star according to min function
-          t_star = self.min_function(betas, min_max)
+          t_star = min_function(betas, min_max)
           #write to disk
           if not os.path.isdir(os.path.dirname(t_star_write_path)):
                   os.path.makedir(os.path.dirname(t_star_write_path))
