3f257e5536d6a2f5efaba908c1abce7fb46f21d8
jgerardsimcock <justin_simcock@yahoo.com>
2017-09-16 15:46:51 -0700

    debug sublcass inheritance

--- mins.py
+++ mins.py
@@ -3,6 +3,9 @@ import numpy as np
 from toolz import memoize
 import warnings
 import os
+from impact import Impact
+
+
 
 
 def _findpolymin(coeffs, min_max):
@@ -64,6 +67,10 @@ def minimize_polynomial(da, dim='prednames', bounds=None):
 
 
     '''
+    print(da)
+
+    da = da.copy()
+
     print(da)
 
     t_star_values = np.apply_along_axis(_findpolymin, da.get_axis_num(dim), da, min_max=bounds)
