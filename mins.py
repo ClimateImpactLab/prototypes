2d04ce19a68d9832f67fe05f46ba32372d9ac5ca
jgerardsimcock <justin_simcock@yahoo.com>
2017-09-16 17:24:47 -0700

    debug sublcass inheritance

--- mins.py
+++ mins.py
@@ -71,7 +71,7 @@ def minimize_polynomial(da, dim='prednames', bounds=[10,25]):
     print(bounds)
     t_star_values = np.apply_along_axis(_findpolymin, 1, da, min_max=bounds)
 
-    if t_star_values.shape != tuple([s for i, s in enumerate(da.shape) if i != da.get_axis_num(dim)]):
+    if t_star_values.shape != tuple([s for i, s in enumerate(da.shape) if i != 1]):
         raise ValueError('_findpolymin returned an unexpected shape: {}'.format(t_star_values.shape))
 
     t_star = xr.DataArray(
@@ -79,7 +79,7 @@ def minimize_polynomial(da, dim='prednames', bounds=[10,25]):
         dims=tuple([d for d in da.dims if d != dim]),
         coords={c: da.coords[c] for c in da.coords.keys() if c != dim})
 
-    t_star = t_star.expand_dims(dim, axis=da.get_axis_num(dim))
+    t_star = t_star.expand_dims(dim, axis=1)
 
     # this is the only part I'm unsure of. Should the 0th term be included?
     t_star_poly = xr.concat([t_star**i for i in range(len(da.coords[dim]))], dim=da.coords[dim])
