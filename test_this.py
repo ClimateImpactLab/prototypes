624ebee74e6a06bcb26be7a38f8ca778415ffdaf
jgerardsimcock <justin_simcock@yahoo.com>
2017-09-18 12:43:54 -0700

    need to test

--- test_this.py
+++ test_this.py
@@ -25,14 +25,15 @@ def test_this():
 	# betas = m._compute_betas(gammas, [gdp, climtas])
 
 
-	impact  = m.compute(gammas, gdp, climtas, min_max_boundary=[10,25], t_star_write_path=t_star_path) 
+	impact  = m.compute(gammas, gdp, climtas, bounds=[10,25], t_star_path=t_star_path) 
+	baseline = xr.open_dataset('/global/scratch/jsimcock/gcp/impacts/mortality-daily/median/historical/low/SSP1/ACCESS1-0/baseline/0.1.1.nc')
 
 
 	t2 = time.time()
 
 	print(t2-t1)
 
-	return impact
+	return impact, baseline
 
 
 
