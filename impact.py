053cec2fb9dde4ae8a5d5547adea528336fb791c
jgerardsimcock <justin_simcock@yahoo.com>
2017-09-18 20:20:23 -0700

    concat to dataarray

--- impact.py
+++ impact.py
@@ -77,7 +77,7 @@ class Impact(object):
 
     '''
     
-    ones = [xr.DataArray(np.ones(len(gdp_covar.hierid)), coords={'hierid': gdp_covars.hierid}, dims=['hierid'], name='1')]
+    ones = xr.DataArray(np.ones(len(gdp_covar.hierid)), coords={'hierid': gdp_covar.hierid}, dims=['hierid'], name='1')
     covars = xr.concat([ones, clim_covar, gdp_covar], pd.Index(gammas.data_vars.keys(), name='covarnames'))
 
 
