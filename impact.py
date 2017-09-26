9294e4a4ee3ce9c6df2a9fde86cd4ec18e9f25fa
Michael Delgado <delgadom@users.noreply.github.com>
2017-09-25 21:07:24 -0700

    simplify / reduce interface
    move all mortality-specific & mathematical operations into script

--- impact.py
+++ impact.py
@@ -3,71 +3,18 @@ import xarray as xr
 import pandas as pd
 import numpy as np
 from toolz import memoize
-from mins import minimize_polynomial
+from prototypes.mins import minimize_polynomial
 import time
 
 
 class Impact(object):
     '''
     Base class for computing an impact as specified by the Climate Impact Lab
+    
     '''
 
     min_function = NotImplementedError
 
-    def __init__(self, weather, preds, metadata):
-        '''
-        Parameters
-        ----------
-        weather: str
-        String representation of the unformatted file paths for annual weather
-
-        preds: list
-        list of strings representing the values to complete formatting for annual weather
-
-        metadata: dict
-        metadata for this impact run
-
-        Returns
-        -------
-        None
-        '''
-        self.preds = preds
-        self.weather = self.get_weather(weather, metadata)
-
-    def get_weather(self, weather, metadata):
-        '''
-        Constructs the annual weather dataarray for a given years impact
-
-        Parameters
-        ----------
-        weather: str representation of unformatted file paths
-
-        metadata: dict
-          values to populate file string
-
-        Returns
-        -------
-
-          Dataarray
-            :py:class `~xarray Datarray` with each weather variable as an `coord`
-        '''
-
-        weather_files = [weather.format(scenario=metadata['scenario'], 
-                                    model=metadata['model'], 
-                                    year=metadata['year'], 
-                                    pred=pred) for pred in self.preds]
-
-        weathers = []
-        for file in weather_files:
-            with xr.open_dataset(file) as ds:
-                ds.load()
-            varname = ds.variable
-            weathers.append(ds[varname])
-
-        annual_weather = xr.concat(weathers, pd.Index(self.preds, name='prednames'))
-
-
-        return annual_weather
 
     def impact_function(self, betas, weather):
         '''
@@ -94,88 +41,72 @@ class Impact(object):
         '''
     
         return (betas*weather).sum(dim='prednames')
+    
+    def _combine_dataarrays(self, name, **arrays):
+        
+        array_names = arrays.keys()
+        array_values = [arrays[k] for k in array_names]
 
-
-    ####################
-    # to be deprecated #
-    ####################  
-    def _construct_covars(self, gdp_covar, clim_covar):
+        return xr.concat(
+            array_values,
+            pd.Index(array_names, name=name))
+    
+    def combine_weather(self, **weather):
         '''
         Helper function to construct the covariates dataarray
 
         Parameters
         -----------
-        gdp_covar: :py:class:`~xarray.DataArray`
-          hierid by gdp dataarray
-
-        clim_covar: :py:class:`~xarray.DataArray`
-          hierid by clim dataarray
+        weather: keyword arguments of DataArrays
+          weather :py:class:`~xarray.DataArray`s
 
         Returns
         -------
-          Dataarray
-               heirid by covars (1, gdp, clim) :py:class:`~xarray.DataArray`
+        combined: DataArray
+            Combined :py:class:`~xarray.DataArray` of weather
+            variables, with variables concatenated along the
+            new `prednames` dimension
         '''
+        
+        return self._combine_dataarrays(name='prednames', **weather)
 
-        #this will change once we generalize
-        for varname in ['iso']:
-            if varname in gdp_covar:
-                gdp_covar = gdp_covar.drop(varname)
-
-        gdp = gdp_covar.rename('loggdppc')
-        climtas = clim_covar.rename('climtas')
-
-        ones = xr.DataArray(
-            np.ones(len(gdp.hierid)),
-            coords={'hierid': gdp.hierid},
-            dims=['hierid'], name='1')
-
-        cv = [ones, gdp, climtas]
-        covars = xr.concat(
-            cv,
-            pd.Index([i.name for i in cv], name='covarnames'))
-
-        return covars
-
-    ####################
-    # to be deprecated #
-    ####################  
-    def compute_betas(self, gammas, gdp_covar, clim_covar):
+    def combine_covars(self, add_constant=True, **covars):
         '''
-        Computes the matrices beta*gamma x IR for each covariates 
+        Helper function to construct the covariates dataarray
 
         Parameters
-        ----------
-        gammas: :py:class `~xarray.Dataset`
-            Coefficients for pred/covar combo
-
-        gdp_covar: :py:class:`~xarray.DataArray`
-            hierid by gdp dataarray
-
-        clim_covar: :py:class:`~xarray.DataArray`
-            hierid by clim dataarray    
+        -----------
+        add_constant : bool
+            flag indicating whether a constant term should be added. The constant term will have the
+            same shape as the other covariate DataArrays
+        
+        covars: keyword arguments of DataArrays
+          covariate :py:class:`~xarray.DataArray`s
 
         Returns
         -------
-        Dataarray
-            hierid by outcome and predname :py:class `~xarray.Dataarray` 
+        combined: DataArray
+            Combined :py:class:`~xarray.DataArray` of covariate
+            variables, with variables concatenated along the
+            new `covarnames` dimension
         '''
-        covars = self._construct_covars(gdp_covar, clim_covar)
 
-        betas = (gammas*covars).sum(dim='covarnames')
+        if add_constant:
+            ones = xr.DataArray(np.ones(shape=covars.values()[0].shape),
+                coords=covars.values()[0].coords,
+                dims=covars.values()[0].dims)
+            covars['1'] = ones
+            
+        return self._combine_dataarrays(name='covarnames', **covars)
 
-        return betas
-
-
-    def compute(self,  
+    def compute(self,
+            weather,
             gammas, 
-            gdp_covar,
-            clim_covar,
-            baseline,
+            covars,
+            baseline=0,
             bounds=None,
-            t_star_path=None,
-            postprocess_daily=False,
-            postprocess_annual=False):
+            clip_flat_curve=True,
+            t_star=None):
         '''
         Computes an impact for a unique set of gdp, climate, weather and gamma coefficient inputs.
         For each set of these, we take the analytic minimum value between two points, 
@@ -184,30 +115,31 @@ class Impact(object):
 
         Parameters
         ----------
+        
+        weather: DataArray
+          weather :py:class:`~xarray.DataArray`
 
-        gammas: 
-          covarname by outcome py:class: `~xarray.Dataset`
+        gammas: DataArray
+          covarname by outcome py:class:`~xarray.DataArray`
 
-        gdp_covar: :py:class:`~xarray.DataArray`
-          hierid by gdp dataarray
+        gdp_covar: DataArray
+          hierid by gdp :py:class:`~xarray.DataArray`
 
-        clim_covar: :py:class:`~xarray.DataArray`
-          hierid by clim dataarray 
+        clim_covar: DataArray
+          hierid by predname :py:class:`~xarray.DataArray`
 
         baseline: :py:class:`~xarray.Dataset`
           precomputed avg baseline impact for impacts between base years
 
         bounds: list
           list of values to compute mins for computing m-star
+         
+        clip_flat_curve: bool
+          flag indicating that flat-curve clipping should be performed
+          on the result
 
         t_star_path: str
           unformatted string path to designate read/write location for t_star
-
-        postprocess_daily: bool
-          If true execute additional functions in subclasses
-
-        postprocess_annual: bool
-          If true execute additional functions in subclasses
 
 
         Returns
@@ -215,46 +147,33 @@ class Impact(object):
           :py:class `~xarray.Dataset` of impacts by hierid by outcome group 
 
         '''
-        t1 = time.time()
+
         #Generate Betas
-        betas = self.compute_betas(gammas, gdp_covar, clim_covar)
-        t2 = time.time()
-        print('computing betas {}'.format(t2-t1))
+        betas = (gammas*covars).sum(dim='covarnames')
 
         #Compute Raw Impact
-        impact = self.impact_function(betas, self.weather)
-        t3 = time.time()
-        print('computing impact {}'.format(t3-t2))
+        impact = self.impact_function(betas, weather)
 
-        #Compute the min for flat curve adaptation
-        m_star = self._compute_m_star(betas, bounds=bounds, t_star_path=t_star_path)
-        t4 = time.time()
-        print('computing m_star {}'.format(t4-t3))
+        if clip_flat_curve:
 
-        #Compare values and evaluate a max
-        impact = xr.ufuncs.minimum(impact, m_star)
-        t5 =time.time()
-        print('taking min {}'.format(t5-t4))
+            #Compute the min for flat curve adaptation
+            impact_flatcurve = self.impact_function(betas, t_star)
 
-        if postprocess_daily:
-          impact = self.postprocess_daily(impact)
+            #Compare values and evaluate a max
+            impact = xr.ufuncs.minimum(impact, impact_flatcurve)
+
+        impact = self.postprocess_daily(impact)
 
         #Sum to annual
         impact = impact.sum(dim='time')
-        t6 = time.time() 
-        print('annual sum {}'.format(t6 -t5))
 
         impact_annual = impact - baseline
-        t7 = time.time()
-        print('rebased {}'.format(t7 -t6))
 
+        impact_annual = self.postprocess_annual(impact_annual) 
 
-        if postprocess_annual:
-          impact_annual= self.postprocess_annual(impact_annual) 
+        return impact_annual
 
-        return impact_annual.rebased
-
-    def _get_t_star(self, path):
+    def get_t_star(self, path):
         '''
         Read precomputed t_star
 
@@ -264,68 +183,46 @@ class Impact(object):
           place to load t-star from 
 
         '''
-        with xr.open_dataset(path) as ds:
-            ds.load()
-        return ds
+        
+        try:
+            with xr.open_dataset(t_star_path) as t_star:
+                return t_star.load()
 
-    def _compute_m_star(self, betas, bounds=None, t_star_path=None):
-        '''
-        Computes m_star, the value of an impact function for a given set of betas given t_star. 
-        t_star, the value t at which an impact is minimized for a given hierid is precomputed 
-        and used to compute m_star.
+        except OSError:
+            pass
 
-        Parameters
-        ----------
-        betas: :py:class `~xarray.Dataset` 
-            coefficients by hierid Dataset 
+        except (IOError, ValueError):
+            try:
+                os.remove(t_star_path)
+            except:
+                pass
 
-        bounds: list
-            values to evaluate min at 
+        #Compute t_star according to min function
+        t_star = self.compute_t_star(betas, bounds=bounds)
 
-        t_star_path: str
+        #write to disk
+        if not os.path.isdir(os.path.dirname(t_star_path)):
+            os.makedirs(os.path.dirname(t_star_path))
 
-        Returns
-        -------
+        t_star.to_netcdf(t_star_path)
 
-        m_star Dataset
-            :py:class`~xarray.Dataset` of impacts evaluated at tstar. 
-
-
-        .. note:: writes to disk and subsequent calls will read from disk. 
-        '''
-        # if file does not exist create it
-        if not os.path.isfile(t_star_path):
-
-            #Compute t_star according to min function
-            t_star = self.compute_t_star(betas, bounds=bounds)
-
-            #write to disk
-            if not os.path.isdir(os.path.dirname(t_star_path)):
-                os.makedirs(os.path.dirname(t_star_path))
-
-            t_star.to_netcdf(t_star_path)
-
-        else:
-
-            #Read from disk
-            t_star = self._get_t_star(t_star_path)
-
-        return self.impact_function(betas, t_star)
+        return t_star
 
     def compute_t_star(self, betas, bounds=None):
         return self.min_function(betas, bounds=bounds)
 
     def postprocess_daily(self, impact):
-        raise NotImplementedError
+        return impact
 
     def postprocess_annual(self, impact):
-        raise NotImplementedError
+        return impact
 
 
 class PolynomialImpact(Impact):
     '''
     Polynomial-specific Impact spec, with ln(gdppc) and climtas for covariates
     '''
+
     @staticmethod
     def min_function(*args, **kwargs):
         '''
@@ -352,5 +249,3 @@ class PolynomialImpact(Impact):
         '''
 
         return minimize_polynomial(*args, **kwargs)
-
-
