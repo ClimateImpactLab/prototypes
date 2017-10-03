c8d9e23754d2794a0f0f4b9e8bf1528df9188d4b
jgerardsimcock <justin_simcock@yahoo.com>
2017-10-02 19:00:59 -0700

    brc debug

--- iface.py
+++ iface.py
@@ -39,7 +39,7 @@ NO_ADAP_CLIM_COVAR = ('/global/scratch/jsimcock/data_files/covars/climate/hierid
     '{write_scenario}/{model}/{base_year}/0.1.1.nc4')
 
 NO_ADAP_GDP_COVAR = ('/global/scratch/jsimcock/data_files/covars/ssp_kernel_13_gdppc/' +
-    '{ssp}/{econ_model}/{base_year}/0.1.2.nc')
+    '{ssp}/{econ_model}/{base_year}/0.1.3.nc')
 
 GDP_COVAR = ('/global/scratch/jsimcock/data_files/covars/ssp_kernel_13_gdppc/{ssp}/{econ_model}/{year}/0.1.3.nc')
 
